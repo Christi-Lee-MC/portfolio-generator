75c1fb734ff90eddb9cd22eb9546f45e72560f06
Christi-Lee <christileemc@gmail.com>
2021-05-31 09:25:06 -0700

    added HTML generator, cleaned up app.js and created src file and page-template.js

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
 
-// const generatePage=()=> 'Name: Christi-Lee, Github: Christi-Lee-MC';
-// const generatePage=(userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+const generatePage = require(`./src/page-template.js`);
 
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
- 
-console.log(name, github);
-console.log(generatePage(name, github));
-// 
-// console.log(generatePage());
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
