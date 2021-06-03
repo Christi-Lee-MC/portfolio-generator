56f433f08c85561352a2e32fb1a07707d4e86aa8
Christi-Lee <christileemc@gmail.com>
2021-06-02 20:53:40 -0700

    copied class code per instructions

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
-
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -132,12 +131,9 @@ promptUser()
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
-      
     fs.writeFile('./index.html', pageHTML, err => {
       if (err) throw new Error(err);
+
       console.log('Page created! Check out index.html in this directory to see it!');
     });
   });
-    
- 
-
